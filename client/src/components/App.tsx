302304399842e4cc743d4e3e640945ba35adb375
James Geer <jamesrgeer@hotmail.co.uk>
2022-07-05 14:50:33 +0100

    basic React setup and temporary backend folder

---
+++ client/src/components/App.tsx
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import Home from './pages/Home';
+
+export default function App() {
+	return (
+		<Router>
+			<Routes>
+				<Route path="/" element={<Home />} />
+			</Routes>
+		</Router>
+	);
+}
---
+++ server/server.js
@@ -0,0 +1,29 @@
+import Koa from 'koa';
+import Router from 'koa-router';
+import BodyParser from 'koa-bodyparser';
+import Logger from 'koa-logger';
+import cors from 'koa-cors';
+import HttpStatus from 'http-status';
+
+const app = new Koa();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(BodyParser());
+app.use(Logger());
+app.use(cors());
+
+const router = new Router();
+
+router.get('/book', async (ctx, next) => {
+	const books = ['Speaking javascript', 'Fluent Python', 'Pro Python', 'The Go programming language'];
+	ctx.status = HttpStatus.OK;
+	ctx.body = books;
+	await next();
+});
+
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(PORT, () => {
+	console.log('==> 🌎  Listening on port %s. Visit http://localhost:%s/', PORT, PORT);
+});
