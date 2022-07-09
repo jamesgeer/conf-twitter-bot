61338ecaf90613c37d6286a41a49cb454fa1c19d
James Geer <jamesrgeer@hotmail.co.uk>
2022-07-09 18:06:29 +0100

    login form event handlers and basic validation

--- frontend/src/components/pages/Login.tsx
+++ frontend/src/components/pages/Login.tsx
@@ -1,4 +1,37 @@
+import React, { useState } from 'react';
+
 const Login = () => {
+	const [password, setPassword] = useState('');
+	const [validationError, setValidationError] = useState(false);
+	const [errorText, setErrorText] = useState('');
+
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setPassword(e.target.value);
+
+		// reset validation errors when password input is modified
+		if (validationError) {
+			setValidationError(false);
+		}
+	};
+
+	// submit form if user hits the enter key on their keyboard
+	const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+		if (e.key === 'Enter') {
+			handleSubmission(e);
+		}
+	};
+
+	// submission from input itself or from button
+	const handleSubmission = (e: React.FormEvent<HTMLInputElement> | React.MouseEvent<HTMLButtonElement>) => {
+		e.preventDefault();
+
+		// check submission length
+		if (password.length === 0) {
+			setValidationError(true);
+			setErrorText('Please enter a password.');
+		}
+	};
+
 	return (
 		<div className="w-full max-w-xs">
 			<form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
@@ -7,17 +40,23 @@ const Login = () => {
 						Password
 					</label>
 					<input
-						className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+						className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline ${
+							validationError ? 'border-red-500' : ''
+						}`}
 						id="password"
 						type="password"
 						placeholder="Password"
+						value={password}
+						onChange={(e) => handleChange(e)}
+						onKeyDown={(e) => handleKeyDown(e)}
 					/>
-					<p className="text-red-500 text-xs italic">Please enter a password.</p>
+					<p className={`text-red-500 text-xs italic ${validationError ? 'block' : 'hidden'}`}>{errorText}</p>
 				</div>
 				<div className="flex items-center justify-between">
 					<button
 						className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full"
 						type="button"
+						onClick={(e) => handleSubmission(e)}
 					>
 						Sign In
 					</button>
