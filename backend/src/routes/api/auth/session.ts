e88b33cd683e7328e62786f3c4fd9bf2b3c0249d
James Geer <jamesrgeer@hotmail.co.uk>
2022-07-10 14:28:17 +0100

    debating api paths

--- backend/src/routes/api/auth/session.ts
+++
@@ -1,9 +0,0 @@
-/**
- * ---
- * GET: /api/auth/session
- * True if session exists
- * ---
- * POST: /api/auth/session
- * Create new session
- * ---
- */
---
+++ backend/src/routes/api/session.ts
@@ -0,0 +1,12 @@
+/**
+ * ---
+ * GET: /api/auth/session
+ * Get session (True if exists)
+ * ---
+ * POST: /api/auth/session/new
+ * Create session (App login)
+ * ---
+ * POST: /api/auth/session/delete
+ * Delete session (App logout)
+ * ---
+ */
