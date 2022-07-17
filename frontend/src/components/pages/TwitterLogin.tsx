f70bf8dcf0c2bf2ad5ebe832d64b0b4535e3a012
James Geer <jamesrgeer@hotmail.co.uk>
2022-07-17 12:38:12 +0100

    refactored twitter login component: comments, clean up and error msgs

--- frontend/src/components/pages/TwitterLogin.tsx
+++ frontend/src/components/pages/TwitterLogin.tsx
@@ -1,44 +1,84 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import queryString from 'query-string';
+import HttpStatus from 'http-status';
+
+interface TwitterAccount {
+	userId: string;
+	screenName: string;
+	profileImageUrl: string;
+}
 
 const TwitterLogin = () => {
 	const [isLoggedIn, setIsLoggedIn] = useState(false);
-	const [name, setName] = useState();
-	const [imageUrl, setImageUrl] = useState();
-	const [status, setStatus] = useState();
-	const [url, setUrl] = useState();
+	const [errorMessage, setErrorMessage] = useState('');
 
-	useEffect(() => {
-		(async () => {
-			const { oauth_token, oauth_verifier } = queryString.parse(window.location.search);
-			console.log(oauth_token);
-			console.log(oauth_verifier);
+	// temp
+	const [userId, setUserId] = useState('');
+	const [screenName, setScreenName] = useState('');
+	const [profileImageUrl, setProfileImageUrl] = useState('');
 
-			if (oauth_token && oauth_verifier) {
-				try {
-					const config = { withCredentials: true };
-					const payload = { oauth_token, oauth_verifier };
-					await axios.post('/api/twitter/oauth/access_token', payload, config);
-				} catch (error) {
-					console.error(error);
-				}
-			}
-		})();
-	}, []);
+	useEffect(() => {}, []);
 
-	const login = async () => {
+	// OAuth Step 1: get request token from backend
+	const getOAuthRequestToken = async () => {
 		try {
-			// OAuth Step 1: retrieve request token from backend
 			const response = await axios.get('/api/twitter/oauth/request_token');
-			const { oauthToken } = response.data;
+			const { oauthToken: oAuthToken } = response.data;
 
-			// Oauth Step 2: direct user to Twitter's login page
-			window.location.href = `https://api.twitter.com/oauth/authenticate?oauth_token=${oauthToken}`;
+			if (oAuthToken.length > 0) {
+				navigateToTwitterLogin(oAuthToken);
+				return;
+			}
 		} catch (error) {
 			console.error(error);
 		}
+
+		setErrorMessage('Unable to authenticate with server, please try again.');
 	};
+
+	// Oauth Step 2: direct user to Twitter's login page
+	const navigateToTwitterLogin = (oAuthToken: string) => {
+		window.location.href = `https://api.twitter.com/oauth/authenticate?oauth_token=${oAuthToken}`;
+	};
+
+	// Oauth Step 3: parse access token from url and post to backend
+	// if successful the user details will be returned
+	const processAccessToken = async () => {
+		const { oauth_token, oauth_verifier } = queryString.parse(window.location.search);
+		if (oauth_token && oauth_verifier) {
+			try {
+				const config = { withCredentials: true };
+				const payload = { oauth_token, oauth_verifier };
+				const response = await axios.post('/api/twitter/oauth/access_token', payload, config);
+
+				if (response.status === HttpStatus.CREATED) {
+					const twitterAccount = response.data;
+					logUserIn(twitterAccount);
+					return;
+				}
+
+				setErrorMessage('Unable to log into Twitter account.');
+			} catch (error) {
+				console.error(error);
+			}
+		}
+
+		setErrorMessage('Failed to authenticate with Twitter, please try again.');
+	};
+
+	// User has been logged in, store account details from response
+	const logUserIn = (twitterAccount: TwitterAccount) => {
+		if (twitterAccount.userId.length > 0) {
+			setUserId(twitterAccount.userId);
+			setScreenName(twitterAccount.screenName);
+			setProfileImageUrl(twitterAccount.profileImageUrl);
+			setIsLoggedIn(true);
+		}
+		setErrorMessage('User details missing from response.');
+	};
+
+	const login = async () => {};
 
 	const logout = async () => {
 		try {
@@ -54,6 +94,7 @@ const TwitterLogin = () => {
 
 	return (
 		<div>
+			{errorMessage.length > 0 && <p className="text-red-600">{errorMessage}</p>}
 			{!isLoggedIn && (
 				<img
 					className="signin-btn"
