fdcf79372102c2dca99afacc0b4cd5529ae0814d
James Geer <jamesrgeer@hotmail.co.uk>
2022-09-18 09:54:28 +0100

    fixed infinite loop if no tweets returned and added message

--- frontend/src/pages/Dashboard/tweets/AllTweets.tsx
+++ frontend/src/pages/Dashboard/tweets/AllTweets.tsx
@@ -7,10 +7,8 @@ const AllTweets = () => {
 	const [tweets, setTweets] = useState<Tweets>([]);
 
 	useEffect(() => {
-		if (tweets.length === 0) {
-			getTweets().then();
-		}
-	}, [tweets]);
+		getTweets().then();
+	}, []);
 
 	const getTweets = async () => {
 		try {
@@ -31,7 +29,7 @@ const AllTweets = () => {
 		);
 	});
 
-	return <div className="grid gap-4">{displayTweets}</div>;
+	return <>{tweets.length > 0 ? <div className="grid gap-4">{displayTweets}</div> : <p>No tweets to display.</p>}</>;
 };
 
 export default AllTweets;
