9e512090a08f90175f0a6cf0feb8c43db74491b0
James Geer <jamesrgeer@hotmail.co.uk>
2022-09-20 17:42:56 +0100

    added unique constraint to twitterUser id and added insert method

--- backend/src/routes/twitter-users/twitter-users-controller.ts
+++ backend/src/routes/twitter-users/twitter-users-controller.ts
@@ -4,17 +4,10 @@ import { getTwitterUser } from './twitter-users-model';
 
 export const twitterUser = async (ctx: ParameterizedContext): Promise<void> => {
 	const { id: twitterUserId } = ctx.params;
-	console.log(twitterUserId);
 	const result = await getTwitterUser(twitterUserId);
-	console.log(result);
 
 	ctx.status = HttpStatus.OK;
 	ctx.body = result;
-};
-
-export const twitterUsers = async (ctx: ParameterizedContext): Promise<void> => {
-	console.log('NOT IMPLEMENTED');
-	ctx.status = HttpStatus.INTERNAL_SERVER_ERROR;
 };
 
 export const createTwitterUser = async (ctx: ParameterizedContext): Promise<void> => {
--- backend/src/routes/twitter-users/twitter-users-model.ts
+++ backend/src/routes/twitter-users/twitter-users-model.ts
@@ -1,5 +1,8 @@
+import HttpStatus from 'http-status';
 import { TwitterUser } from '../accounts/accounts';
 import prisma from '../../../lib/prisma';
+import { ServerError } from '../types';
+import { TwitterAccount } from '../oauths/oauths';
 
 export const getTwitterUser = async (twitterUserId: string): Promise<TwitterUser> =>
 	prisma.twitterUser.findUnique({
@@ -7,3 +10,21 @@ export const getTwitterUser = async (twitterUserId: string): Promise<TwitterUser
 			id: BigInt(twitterUserId),
 		},
 	});
+
+export const insertTwitterUser = async (twitterAccount: TwitterAccount): Promise<boolean | ServerError> => {
+	const { userId, name, screenName, profileImageUrl } = twitterAccount;
+	try {
+		await prisma.twitterUser.create({
+			data: {
+				id: BigInt(userId),
+				name,
+				screenName,
+				profileImageUrl,
+			},
+		});
+	} catch (e) {
+		return new ServerError(HttpStatus.INTERNAL_SERVER_ERROR, 'Unable to add Twitter user due to server problem.');
+	}
+
+	return true;
+};
