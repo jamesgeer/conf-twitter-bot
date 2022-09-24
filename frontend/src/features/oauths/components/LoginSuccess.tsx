34ebd5704852e40fbfad9df214be992d219d5965
James Geer <jamesrgeer@hotmail.co.uk>
2022-09-24 09:50:35 +0100

    added callback route to index (temp), commented out pgadmin

--- frontend/src/features/oauths/components/LoginSuccess.tsx
+++ frontend/src/features/oauths/components/LoginSuccess.tsx
@@ -1,4 +1,12 @@
+import { useEffect } from 'react';
+
 const LoginSuccess = () => {
+	useEffect(() => {
+		setTimeout(() => {
+			window.close();
+		}, 5000);
+	});
+
 	return (
 		<div>
 			<p>Please wait a moment...</p>
--- frontend/src/pages/Index.tsx
+++ frontend/src/pages/Index.tsx
@@ -9,6 +9,7 @@ import Dashboard from '../pages/Dashboard';
 import { AccountContext } from '../context/AccountContext';
 import { AccountContextProps } from '../types';
 import SignUp from './SignUp';
+import LoginSuccess from '../features/oauths/components/LoginSuccess';
 
 export default function Index() {
 	const [appLoggedIn, setAppLoggedIn] = useState(false);
@@ -52,6 +53,7 @@ export default function Index() {
 					<Route path="sign-up" element={<SignUp />} />
 					<Route path="login" element={<Login appLogin={setAppLoggedIn} />} />
 					<Route path="select-account" element={<AccountSelection />} />
+					<Route path="twitter-oauth-callback" element={<LoginSuccess />} />
 					<Route
 						path="/"
 						element={appLoggedIn && twitterLoggedIn ? <Dashboard /> : <Navigate to="login" />}
