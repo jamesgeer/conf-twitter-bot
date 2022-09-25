31e8c6f7f8e51b61854d3fe123ab1716b67d0210
James Geer <jamesrgeer@hotmail.co.uk>
2022-09-25 10:03:58 +0100

    fixed typescript "possible null" and "widow cannot be null" errors

--- frontend/src/features/oauths/components/LoginWindow.tsx
+++ frontend/src/features/oauths/components/LoginWindow.tsx
@@ -1,8 +1,8 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef } from 'react';
 import { getOAuthRequestToken } from '../api/getRequestToken';
 
 const LoginWindow = () => {
-	const [loginWindow, setLoginWindow] = useState(null);
+	const loginWindow = useRef<Window | null>(null);
 	const windowOpen = useRef(false);
 
 	useEffect(() => {
@@ -13,8 +13,8 @@ const LoginWindow = () => {
 
 			if (handleWindow) {
 				console.log(handleWindow.closed);
-				// @ts-ignore
-				setLoginWindow(handleWindow);
+
+				loginWindow.current = handleWindow;
 				windowOpen.current = true;
 			}
 		};
@@ -23,20 +23,11 @@ const LoginWindow = () => {
 		if (!windowOpen.current) {
 			newWindow().then();
 		}
-
-		// @ts-ignore
-		if (loginWindow && loginWindow.closed) {
-			console.log('window was closed');
-			setLoginWindow(null);
-			windowOpen.current = false;
-		}
 	}, [loginWindow]);
 
 	const windowFeatures = (parentWindow: Window, w: number, h: number): string => {
-		// @ts-ignore
-		const y = parentWindow.top.outerHeight / 2 + parentWindow.top.screenY - h / 2;
-		// @ts-ignore
-		const x = parentWindow.top.outerWidth / 2 + parentWindow.top.screenX - w / 2;
+		const y = parentWindow.top!.outerHeight / 2 + parentWindow.top!.screenY - h / 2;
+		const x = parentWindow.top!.outerWidth / 2 + parentWindow.top!.screenX - w / 2;
 		return `toolbar=no, location=no, directories=no, status=no, menubar=no, copyhistory=no, width=${w}, height=${h}, top=${y}, left=${x}`;
 	};
 
@@ -44,15 +35,6 @@ const LoginWindow = () => {
 		const oAuthToken = await getOAuthRequestToken();
 		return `https://api.twitter.com/oauth/authenticate?oauth_token=${oAuthToken}`;
 	};
-
-	// const createNewLoginWindow = async (): Promise<Window | null> => {
-	// 	const features = windowFeatures(window, 500, 250);
-	// 	const url = await twitterLoginUrl();
-	//
-	// 	return window.open(url, '', features);
-	// };
-
-	// @ts-ignore
 
 	return null;
 };
