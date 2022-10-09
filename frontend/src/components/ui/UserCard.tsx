1d8311761eb02cdf2dc18d23d0ad0f05ed84c93e
James Geer <jamesrgeer@hotmail.co.uk>
2022-10-09 10:29:45 +0100

    dropdown menu on menu card and added tabular icons

--- frontend/src/components/ui/UserCard.tsx
+++ frontend/src/components/ui/UserCard.tsx
@@ -1,4 +1,6 @@
 import { TwitterUser } from '../../features/accounts/types';
+import { Avatar, Button, Menu, MenuButton, MenuList, MenuItem, Icon } from '@chakra-ui/react';
+import { IconSwitchHorizontal, IconLogout } from '@tabler/icons';
 
 interface Props {
 	twitterUser: TwitterUser;
@@ -6,11 +8,28 @@ interface Props {
 
 const UserCard = ({ twitterUser }: Props) => {
 	return (
-		<button className="flex items-center px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded-full">
-			<img className="mr-2 w-8 h-8 rounded-full" src={twitterUser.profileImageUrl} alt="user" />
-			{twitterUser.name}
-		</button>
+		<Menu>
+			<MenuButton as={Button} colorScheme="pink">
+				<Avatar src={twitterUser.profileImageUrl} mr="8px" size="sm" />
+				{twitterUser.name}
+			</MenuButton>
+			<MenuList>
+				<MenuItem>
+					<Icon as={IconSwitchHorizontal} />
+					Switch Account
+				</MenuItem>
+				<MenuItem>
+					<Icon as={IconLogout} />
+					Logout
+				</MenuItem>
+			</MenuList>
+		</Menu>
 	);
 };
 
 export default UserCard;
+
+// <button className="flex items-center px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded-full">
+// 	<Avatar src={twitterUser.profileImageUrl} mr="8px" size="sm" />
+// 	{twitterUser.name}
+// </button>
