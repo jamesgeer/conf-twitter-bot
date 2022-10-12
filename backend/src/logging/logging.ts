a3394674812c58bd0e20535dbcc7b497fe7c6baf
Edward-Christian Marin <edwardchristianmarin@yahoo.com>
2022-10-12 23:57:24 +0100

    formatting

--- backend/src/logging/logging.ts
+++ backend/src/logging/logging.ts
@@ -2,11 +2,10 @@ import * as fs from 'fs';
 
 // Function that writes logs to files
 export const logToFile = function (): void {
-
 	const logDateObject = new Date();
-	const logDate =  `${logDateObject.getFullYear()}_${logDateObject.getMonth()}_${logDateObject.getDate()}`;
+	const logDate = `${logDateObject.getFullYear()}_${logDateObject.getMonth()}_${logDateObject.getDate()}`;
 
-	fs.writeFile(`logging/app_logs/${logDate}.txt`, 'test text',{flag:'a+'}, (err) => {
+	fs.writeFile(`logging/app_logs/${logDate}.txt`, 'test text', { flag: 'a+' }, (err) => {
 		if (err) throw err;
 		console.log(`Logged action to file ${logDate}.txt`);
 	});
