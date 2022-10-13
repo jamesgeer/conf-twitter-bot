146a6c05f638fb0f8957b8546e5c7b674c1b9270
brandonscott4 <bscott15@sky.com>
2022-10-13 21:10:10 +0100

    Added styling to delete account button

--- frontend/src/pages/MyMenu.tsx
+++ frontend/src/pages/MyMenu.tsx
@@ -1,6 +1,5 @@
-import { IconDotsVertical } from '@tabler/icons';
+import { IconDotsVertical, IconAlertTriangle } from '@tabler/icons';
 import { Menu, MenuButton, MenuList, MenuItem, Button } from '@chakra-ui/react';
-import { Account } from '../features/accounts/types';
 
 interface Props {
 	isActive: boolean;
@@ -15,10 +14,21 @@ const MyMenu = ({ isActive, handleButtonClick, accountId }: Props) => {
 				onClick={(e) => handleButtonClick(e, accountId)}
 				as={Button}
 				rightIcon={<IconDotsVertical />}
+				bg="none"
+				_hover={{ bg: 'none' }}
+				_active={{ bg: 'none' }}
 			></MenuButton>
 			{isActive && (
 				<MenuList onClick={(e) => e.stopPropagation()}>
-					<MenuItem onClick={() => console.log('Delete account ' + accountId)}>Delete Account</MenuItem>
+					<MenuItem
+						color="red.500"
+						bg="white"
+						_hover={{ bg: 'red.100' }}
+						icon={<IconAlertTriangle />}
+						onClick={() => console.log('Delete account ' + accountId)}
+					>
+						Delete Account
+					</MenuItem>
 				</MenuList>
 			)}
 		</Menu>
