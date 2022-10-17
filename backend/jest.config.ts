0962afb063dffc4425975cb1722f3c9932339fa6
James Geer <jamesrgeer@hotmail.co.uk>
2022-10-17 18:05:48 +0100

    trying to get tests working docker but failing

--- backend/jest.config.ts
+++ backend/jest.config.ts
@@ -1,7 +1,7 @@
 module.exports = {
 	roots: ['<rootDir>'],
 	transform: {
-		'^.+\\.ts?$': 'ts-jest',
+		'^.+\\.tsx?$': 'ts-jest',
 	},
 	testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.ts?$',
 	moduleFileExtensions: ['ts', 'js', 'json', 'node'],
--- backend/src/routes/accounts/__tests__/accounts-controller.test.ts
+++ backend/src/routes/accounts/__tests__/accounts-controller.test.ts
@@ -3,7 +3,8 @@ import supertest from 'supertest';
 import http from 'http';
 import HttpStatus from 'http-status';
 import { app } from '../../../app';
-import { prismaMock } from '../../../../lib/prismaMock';
+import prisma from '../../../../lib/prisma';
+import { insertUser } from '../../users/users-model';
 
 const apptest = supertest(http.createServer(app.callback()));
 
@@ -13,10 +14,22 @@ const apptest = supertest(http.createServer(app.callback()));
 // 	twitterUserId: BigInt(1),
 // };
 
-test('get account route should return 404/not found', async () => {
-	prismaMock.account.findUnique.mockResolvedValue(null);
-
-	const response = await apptest.get('/api/accounts/1');
-	expect(response.status).toEqual(HttpStatus.NOT_FOUND);
-	expect(response.body).toEqual({ message: 'Account not found.' });
+afterAll(async () => {
+	prisma.user.deleteMany();
+	await prisma.$disconnect();
 });
+
+it('should create 1 new user', async () => {
+	console.log('Attempting to create a user');
+	const newUser = await insertUser('Simon Test-man', 'password');
+	expect(newUser).toEqual(true);
+	console.log('✨ one user successfully created!');
+});
+
+// test('get account route should return 404/not found', async () => {
+// 	prismaMock.account.findUnique.mockResolvedValue(null);
+//
+// 	const response = await apptest.get('/api/accounts/1');
+// 	expect(response.status).toEqual(HttpStatus.NOT_FOUND);
+// 	expect(response.body).toEqual({ message: 'Account not found.' });
+// });
