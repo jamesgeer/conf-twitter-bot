af8655aa1a11c79411177d8ec50829309a75650b
James Geer <jamesrgeer@hotmail.co.uk>
2022-10-17 16:51:41 +0100

    added dotenv-package and test database image

--- backend/src/routes/accounts/__tests__/accounts-controller.test.ts
+++ backend/src/routes/accounts/__tests__/accounts-controller.test.ts
@@ -3,10 +3,19 @@ import supertest from 'supertest';
 import http from 'http';
 import HttpStatus from 'http-status';
 import { app } from '../../../app';
+import { prismaMock } from '../../../../lib/prismaMock';
 
 const apptest = supertest(http.createServer(app.callback()));
 
+// const account = {
+// 	id: 1,
+// 	userId: 1,
+// 	twitterUserId: BigInt(1),
+// };
+
 test('get account route should return 404/not found', async () => {
+	prismaMock.account.findUnique.mockResolvedValue(null);
+
 	const response = await apptest.get('/api/accounts/1');
 	expect(response.status).toEqual(HttpStatus.NOT_FOUND);
 	expect(response.body).toEqual({ message: 'Account not found.' });
