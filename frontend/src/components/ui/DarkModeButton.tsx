7abd596ae10b72e68069113a7de05d5100bc7de0
brandonscott4 <bscott15@sky.com>
2022-10-19 17:20:49 +0100

    Updated to include adding and removing of dark class for tailwind

--- frontend/src/components/ui/DarkModeButton.tsx
+++ frontend/src/components/ui/DarkModeButton.tsx
@@ -4,9 +4,19 @@ import { IconMoon, IconSun } from '@tabler/icons';
 const DarkModeButton = () => {
 	const { colorMode, toggleColorMode } = useColorMode();
 
+	const handleHtmlClass = (): any => {
+		if (colorMode === 'light') {
+			document.documentElement.classList.remove('dark');
+			return <IconMoon />;
+		} else {
+			document.documentElement.classList.add('dark');
+			return <IconSun />;
+		}
+	};
+
 	return (
 		<div>
-			<button onClick={toggleColorMode}>{colorMode === 'light' ? <IconMoon /> : <IconSun />}</button>
+			<button onClick={toggleColorMode}>{colorMode === 'light' ? handleHtmlClass() : handleHtmlClass()}</button>
 		</div>
 	);
 };
