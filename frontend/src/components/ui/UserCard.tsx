39f5802d7c38195be586d69fb47a43cbc95ec221
Isabel Bradley <isabel.bradley@blueyonder.co.uk>
2022-10-22 15:34:43 +0100

    Show accounts in user card menu

--- frontend/src/components/ui/UserCard.tsx
+++ frontend/src/components/ui/UserCard.tsx
@@ -1,8 +1,21 @@
-import { TwitterUser } from '../../features/accounts/types';
-import { Avatar, Button, Menu, MenuButton, MenuList, MenuItem, Icon, Center, Flex } from '@chakra-ui/react';
+import { Account, AccountContextProps, TwitterUser } from '../../features/accounts/types';
+import {
+	Avatar,
+	Button,
+	Menu,
+	MenuButton,
+	MenuList,
+	MenuItem,
+	Icon,
+	Center,
+	Flex,
+	MenuDivider,
+} from '@chakra-ui/react';
 import { IconSwitchHorizontal, IconLogout, IconChevronDown } from '@tabler/icons';
-import { Link, useNavigate } from 'react-router-dom';
-import React from 'react';
+import { Link } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { useAccounts } from '../../features/accounts/api/getAccounts';
+import { AccountContext } from '../../features/accounts/context/AccountContext';
 
 interface Props {
 	twitterUser: TwitterUser;
@@ -10,8 +23,42 @@ interface Props {
 }
 
 const UserCard = ({ twitterUser, handleLogout }: Props) => {
+	const { handleAccountChange } = useContext(AccountContext) as AccountContextProps;
+	const { isLoading, error, data: accounts } = useAccounts();
+
+	// displays "loading accounts" while the accounts are being fetched
+	if (isLoading) {
+		return <div>Loading Accounts...</div>;
+	}
+
+	// if there is an error then will display an error message
+	if (error) {
+		return <div>An error occurred: {error.message}</div>;
+	}
+
+	const handleAccountSelection = (accountId: number) => {
+		const account = accounts.find((account) => account.id === accountId);
+		if (account) {
+			handleAccountChange(account);
+		}
+	};
+
+	const displayAccounts = accounts.map((account: Account) => {
+		return (
+			<MenuItem onClick={() => handleAccountSelection(account.id)} key={account.id}>
+				<Avatar
+					src={account.twitterUser.profileImageUrl} //use the profile picture of the twitter account
+					mr="12px"
+					size="sm" // small picture
+				/>
+				{account.twitterUser.name}
+			</MenuItem>
+		);
+	});
+
 	return (
 		<Menu>
+			{/* menu button displays the current "active" account */}
 			<MenuButton as={Button} size="md" rightIcon={<IconChevronDown />}>
 				<Flex>
 					<Center>
@@ -21,7 +68,10 @@ const UserCard = ({ twitterUser, handleLogout }: Props) => {
 				</Flex>
 			</MenuButton>
 
+			{/* list shows options such as other accounts, switch, logout, etc */}
 			<MenuList>
+				{displayAccounts}
+				<MenuDivider />
 				<MenuItem as={Link} to={'select-account'}>
 					<Icon as={IconSwitchHorizontal} mr="8px" />
 					Switch Account
