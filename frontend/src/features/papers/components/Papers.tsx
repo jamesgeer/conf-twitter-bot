06c83a152e9e66002586dc5d62e9d2fbf5100940
brandonscott4 <bscott15@sky.com>
2022-11-01 23:03:56 +0000

    improved visibility for authors dark mode text

--- frontend/src/features/papers/components/Papers.tsx
+++ frontend/src/features/papers/components/Papers.tsx
@@ -21,7 +21,7 @@ const Papers = ({ isList }: Props) => {
 			<div key={index} className="border-b border-slate-200 pb-4">
 				<header>
 					<h5 className="font-bold">{paper.title}</h5>
-					<small className="text-slate-700">{paper.authors.join(', ')}</small>
+					<small className="text-slate-700 dark:text-slate-400">{paper.authors.join(', ')}</small>
 				</header>
 				<div className="content pt-4">
 					<p>{paper.shortAbstract}</p>
