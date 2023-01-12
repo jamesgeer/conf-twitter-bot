8bbb21feb06ad0af1aa764b4b19ad76604bdd215
James Geer <jamesrgeer@hotmail.co.uk>
2023-01-12 17:20:16 +0000

    fixed upload modal problem by moving code to new component

--- frontend/src/features/uploads/components/DisplayUploads.tsx
+++ frontend/src/features/uploads/components/DisplayUploads.tsx
@@ -1,37 +1,13 @@
 import { Upload, Uploads } from '../types';
-import { SimpleGrid, Box, useDisclosure, Image } from '@chakra-ui/react';
-import { Modal, ModalOverlay, ModalContent, ModalCloseButton } from '@chakra-ui/react';
+import { SimpleGrid } from '@chakra-ui/react';
+import UploadModal from './UploadModal';
 
 const DisplayUploads = ({ uploads }: { uploads: Uploads }) => {
-	const { isOpen, onOpen, onClose } = useDisclosure();
-
-	const media = uploads.map((upload: Upload) => {
-		return (
-			<Image
-				key={upload.name}
-				objectFit="cover"
-				borderRadius="1rem"
-				minHeight="100%"
-				src={upload.url}
-				alt={upload.alt}
-				onClick={onOpen}
-			/>
-		);
-	});
-
-	/*
-					<Modal isOpen={isOpen} onClose={onClose}>
-					<ModalOverlay />
-					<ModalContent maxWidth={'fit-content'}>
-						<ModalCloseButton />
-						<Image src={upload.url} alt={upload.alt} />
-					</ModalContent>
-				</Modal>
-	 */
-
 	return (
 		<SimpleGrid columns={4} spacing={10} paddingTop={'1rem'}>
-			{media}
+			{uploads.map((upload: Upload) => (
+				<UploadModal upload={upload} />
+			))}
 		</SimpleGrid>
 	);
 };
---
+++ frontend/src/features/uploads/components/UploadModal.tsx
@@ -0,0 +1,29 @@
+import { Image, useDisclosure } from '@chakra-ui/react';
+import { Upload } from '../types';
+import { Modal, ModalOverlay, ModalContent, ModalCloseButton } from '@chakra-ui/react';
+
+const UploadModal = ({ upload }: { upload: Upload }) => {
+	const { isOpen, onOpen, onClose } = useDisclosure();
+	return (
+		<>
+			<Image
+				key={upload.name}
+				objectFit="cover"
+				borderRadius="1rem"
+				minHeight="100%"
+				src={upload.url}
+				alt={upload.alt}
+				onClick={onOpen}
+			/>
+			<Modal isOpen={isOpen} onClose={onClose}>
+				<ModalOverlay />
+				<ModalContent maxWidth={'fit-content'}>
+					<ModalCloseButton />
+					<Image src={upload.url} alt={upload.alt} />
+				</ModalContent>
+			</Modal>
+		</>
+	);
+};
+
+export default UploadModal;
