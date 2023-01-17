c3baab26f05f4f26c44bde24a296bcf4a79b9735
James Geer <jamesrgeer@hotmail.co.uk>
2023-01-17 11:28:12 +0000

    fix: upload input needed a unique id

--- frontend/src/features/tweets/components/Tweet/TweetMediaButtons.tsx
+++ frontend/src/features/tweets/components/Tweet/TweetMediaButtons.tsx
@@ -1,6 +1,7 @@
 import { Input, FormLabel, useColorMode, FormControl, FormErrorMessage } from '@chakra-ui/react';
 import React, { useState } from 'react';
 import { IconPhoto } from '@tabler/icons';
+import uuid from 'react-uuid';
 
 interface Props {
 	media: File[] | undefined;
@@ -11,6 +12,7 @@ const TweetMediaButtons = ({ media, setMedia }: Props) => {
 	const [formError, setFormError] = useState({ error: false, message: '' });
 
 	const { colorMode } = useColorMode();
+	const inputId = uuid();
 
 	const handleUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
 		const { files } = e.target;
@@ -35,12 +37,12 @@ const TweetMediaButtons = ({ media, setMedia }: Props) => {
 		<div className="pt-4">
 			<FormControl isInvalid={formError.error} className="inline-block">
 				{/* FormLabel acts as a button for file input */}
-				<FormLabel htmlFor="attachMedia" cursor="pointer">
+				<FormLabel htmlFor={inputId} cursor="pointer">
 					{/* Not sure why chakra does not handle dark mode here*/}
 					<IconPhoto color={colorMode === 'dark' ? 'white' : 'black'} />
 				</FormLabel>
 				<Input
-					id="attachMedia"
+					id={inputId}
 					type="file"
 					name="files[]"
 					accept="image/*"
