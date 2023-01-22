467ee0b47c02568055640c8eda819b61f408aba7
Edward-Christian Marin <edwardchristianmarin@yahoo.com>
2023-01-22 13:53:00 +0000

    add initial scraping code (not working atm)

--- backend/src/routes/scraper/scraper-model.ts
+++ backend/src/routes/scraper/scraper-model.ts
@@ -13,6 +13,8 @@ export async function scrapePapers(urls: string): Promise<boolean> {
 			if (await isAcmUrl(url.trim())) {
 				// TODO: for testing purposes just console log now
 				scrapeListOfAcmPapers(url.trim()).then((r) => console.log(r));
+			} else if (await isRschrUrl(url.trim())) {
+				scrapeListOfRschrPapers(url.trim()).then((r) => console.log(r));
 			}
 		}
 		return true;
@@ -25,6 +27,14 @@ export async function scrapePapers(urls: string): Promise<boolean> {
 
 async function isAcmUrl(url: string): Promise<boolean> {
 	return url.includes('/dl.acm.org/');
+}
+
+async function isRschrUrl(url: string): Promise<boolean> {
+	// every single conference on researchr seems to have its own domain or a different one to the others
+	// the only thing they have in common, is that the papers tab always end with #event-overview
+	// other than that, there'll only be a preliminary check while scraping to make sure that the page
+	// is actually correct
+	return url.endsWith('#event-overview');
 }
 
 // returns true if successfully scraped, false otherwise
@@ -80,7 +90,59 @@ async function scrapeListOfAcmPapers(url: string): Promise<boolean> {
 		await browser.close();
 	}
 }
+// returns true if successfully scraped, false otherwise
+async function scrapeListOfRschrPapers(url: string): Promise<boolean> {
+	// there's also playwright.firefox , we'll need to compare them at a later date for performance/memory
+	const browser = await playwright.chromium.launch({
+		headless: true, // setting this to true will not run the UI
+	});
+	try {
+		// opens a page
+		const page = await browser.newPage();
 
+		// goes to that URL | TODO: error catching
+		await page.goto(url);
+
+		// TODO: also grab the DOI link for automatic full page scraping
+		const paperTypes = await page.$$('.issue-heading');
+		const paperTitleHTags = await page.$$('.issue-item__title');
+		// TODO: click the "+" symbol if it exists for all authors to open
+		const authorContainers = await page.$$('[aria-label=authors]');
+		const dateAndPages = await page.$$('.issue-item__detail');
+		const shortAbstracts = await page.$$('.issue-item__abstract');
+		const citations = await page.$$('span.citation');
+		const downloads = await page.$$('span.metric');
+
+		const numPapers = paperTypes.length;
+		const papers: Papers = [];
+		console.assert(numPapers === paperTitleHTags.length && numPapers === authorContainers.length);
+
+		for (let i = 0; i < numPapers; i += 1) {
+			try {
+				papers.push(
+					// eslint-disable-next-line no-await-in-loop
+					await extractRschrPaper(
+						authorContainers,
+						i,
+						dateAndPages,
+						paperTypes,
+						paperTitleHTags,
+						shortAbstracts,
+						citations,
+						downloads,
+					),
+				);
+			} catch (e) {
+				// ignore entry
+			}
+		}
+		return await uploadPapersToDatabase(papers);
+	} catch (error) {
+		return false;
+	} finally {
+		await browser.close();
+	}
+}
 async function uploadPapersToDatabase(papers: Papers): Promise<boolean> {
 	if (papers.length === 0) {
 		return false;
@@ -114,6 +176,48 @@ async function uploadPapersToDatabase(papers: Papers): Promise<boolean> {
 }
 
 async function extractAcmPaper(
+	authorContainers: ElementHandle<SVGElement | HTMLElement>[],
+	i: number,
+	dateAndPages: ElementHandle<SVGElement | HTMLElement>[],
+	paperTypes: ElementHandle<SVGElement | HTMLElement>[],
+	paperTitleHTags: ElementHandle<SVGElement | HTMLElement>[],
+	shortAbstracts: ElementHandle<SVGElement | HTMLElement>[],
+	citations: ElementHandle<SVGElement | HTMLElement>[],
+	downloads: ElementHandle<SVGElement | HTMLElement>[],
+): Promise<Paper> {
+	// GRAB AUTHORS
+	const authors = await authorContainers[i].$$eval('li a', (authorElm) => {
+		const data: string[] = [];
+		authorElm.forEach((elm) => {
+			if (elm.textContent != null) data.push(elm.textContent);
+		});
+		return data;
+	});
+
+	const spans = await dateAndPages[i].$$('span');
+	// const monthYear = await spans[0].textContent().then((data) => data?.replace(', ', ''));
+	const href = await paperTitleHTags[i].$eval('a', (hrefElm) => hrefElm.href);
+	let paperType = await paperTypes[i].textContent();
+	if (paperType == null) paperType = '';
+	let title = await paperTitleHTags[i].textContent();
+	if (title == null) title = '';
+	let pages = await spans[0].textContent();
+	if (pages == null) pages = '';
+
+	return {
+		type: paperType,
+		title,
+		url: href,
+		doi: href?.replace('https://dl.acm.org/doi', ''),
+		authors,
+		// monthYear,
+		pages,
+		shortAbstract: (await shortAbstracts[i].innerText()).trim(),
+		citations: Number(await citations[i].textContent()),
+		downloads: Number(await downloads[i].textContent()),
+	};
+}
+async function extractRschrPaper(
 	authorContainers: ElementHandle<SVGElement | HTMLElement>[],
 	i: number,
 	dateAndPages: ElementHandle<SVGElement | HTMLElement>[],
