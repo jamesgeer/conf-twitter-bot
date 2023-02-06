1fa0ea6a2475c5d8483a8d5abc4a67bf2eba64fc
James Geer <jamesrgeer@hotmail.co.uk>
2023-02-06 18:13:28 +0000

    cleanup schedule

--- backend/src/jobs/cleanup/__tests__/cleanup.test.ts
+++ backend/src/jobs/cleanup/__tests__/cleanup.test.ts
@@ -1,10 +1,10 @@
 import supertest from 'supertest';
 import HttpStatus from 'http-status';
 import http from 'http';
-import { cleanUp } from '../cleanup';
+import { getFilesToBeDeleted } from '../cleanup';
 import { TestHarness } from '../../../tests/TestHarness';
 import { app } from '../../../app';
-import { deleteUploadDb } from '../../../routes/uploads/uploads-model';
+import { deleteUploadDb, updateUploadUrl } from '../../../routes/uploads/uploads-model';
 import { Upload } from '../../../routes/uploads/uploads';
 
 const request = supertest(http.createServer(app.callback()));
@@ -31,7 +31,36 @@ it('file exists in uploads folder but not in database so should be deleted', asy
 	const deleteResult = <Upload>await deleteUploadDb(upload.id);
 	expect(deleteResult.id).toEqual(upload.id);
 
-	const result = await cleanUp();
-	expect(result.length).toEqual(1);
+	const result = await getFilesToBeDeleted();
 	expect(result.pop()).toEqual(upload.name);
+});
+
+it('file in database has a url set to an address that differs from this application, should be deleted', async () => {
+	const tweet = await harness.createTweet();
+	const uploadResponse = await request
+		.post(uploadsEndpoint)
+		.attach('tweetId', tweet.id)
+		.attach('media', testImage('cleanup_image_1'));
+	expect(uploadResponse.status).toEqual(HttpStatus.OK);
+
+	const upload: Upload = uploadResponse.body.pop();
+	const updateResult = <Upload>await updateUploadUrl(upload.id, 'https://pbs.twimg.com/');
+	expect(updateResult.id).toEqual(upload.id);
+
+	const result = await getFilesToBeDeleted();
+	expect(result.includes(upload.name)).toBe(true);
+});
+
+it('upload should not be included in list to be deleted', async () => {
+	const tweet = await harness.createTweet();
+	const uploadResponse = await request
+		.post(uploadsEndpoint)
+		.attach('tweetId', tweet.id)
+		.attach('media', testImage('cleanup_image_1'));
+	expect(uploadResponse.status).toEqual(HttpStatus.OK);
+
+	const upload: Upload = uploadResponse.body.pop();
+
+	const result = await getFilesToBeDeleted();
+	expect(result.includes(upload.name)).toBe(false);
 });
--- backend/src/jobs/cleanup/cleanup.ts
+++ backend/src/jobs/cleanup/cleanup.ts
@@ -1,6 +1,8 @@
 import { readdirSync } from 'fs';
 import { getAllUploads } from './cleanup-model';
 import { uploadFolder } from '../../routes/util';
+import { APP_URL } from '../../keys';
+import { deleteUploadFile } from '../../routes/uploads/uploads-model';
 
 const getUploadedMedia = async (): Promise<string[]> =>
 	// get all files but ignore directories and the gitignore file
@@ -8,11 +10,30 @@ const getUploadedMedia = async (): Promise<string[]> =>
 		.filter((item) => !item.isDirectory() && item.name !== '.gitignore')
 		.map((item) => item.name);
 
-export const cleanUp = async (): Promise<string[]> => {
+export const getFilesToBeDeleted = async (): Promise<string[]> => {
 	const filenames = await getUploadedMedia();
 	const dbEntries = await getAllUploads();
 
-	// compares the list of filenames to the list of db entries, any filename that is not in
-	// dbEntries is returned in a new list
-	return filenames.filter((file) => !dbEntries.find((entry) => entry.name === file));
+	// returns a new list containing filenames that are not in the database, or if the url is different
+	// from the one set for this application (so image is hosted elsewhere)
+	return filenames.filter((filename) => {
+		const matchingEntry = dbEntries.find((entry) => entry.name === filename);
+		return !matchingEntry || !matchingEntry.url.includes(APP_URL);
+	});
 };
+
+const cleanupSchedule = async (): Promise<void> => {
+	const files = await getFilesToBeDeleted();
+
+	if (files.length === 0) {
+		return;
+	}
+
+	for (const file of files) {
+		await deleteUploadFile(file)
+			.then(() => console.log(`CLEANUP_CRON: Deleted ${file}`))
+			.catch((error) => console.error(`CLEANUP_CRON: Error deleting ${file}: ${error}`));
+	}
+};
+
+export default cleanupSchedule;
